78ba29c4728668caff37048ce99688fad209d77f
Sergey Drozdkov <sergey@getintently.ai>
2023-11-24 13:05:48 +0300

    Fix dropdown menu

--- dojo/tools/detect_secrets/parser.py
+++ dojo/tools/detect_secrets/parser.py
@@ -1,8 +1,6 @@
-import json
 import hashlib
-import dateutil.parser
+import json
 from dojo.models import Finding
-
 
 class DetectSecretsParser(object):
     """
@@ -19,46 +17,42 @@ class DetectSecretsParser(object):
         return "Import JSON output for detect-secrets scan report."
 
     def get_findings(self, filename, test):
-        data = json.load(filename)
-        dupes = {}
-        if data.get("generated_at"):
-            find_date = dateutil.parser.parse(data.get("generated_at"))
-        for detect_file in data.get("results"):
-            for item in data.get("results").get(detect_file):
-                type = item.get("type")
-                file = item.get("filename")
-                hashed_secret = item.get("hashed_secret")
-                is_verified = item.get("is_verified")
-                line = item.get("line_number")
-                description = "Detected potential secret with the following related data:\n"
-                description += "**Filename:** " + file + "\n"
-                description += "**Line:** " + str(line) + "\n"
-                description += "**Type:** " + type + "\n"
+        try:
+            file_contents = filename.read().decode('utf-8')
+            issues = json.loads(file_contents)
+        except Exception as e:
+            raise ValueError(f"Failed to parse JSON: {str(e)}")
 
-                dupe_key = hashlib.sha256(
-                    (type + file + str(line) + hashed_secret).encode("utf-8")
-                ).hexdigest()
+        findings = []
 
-                if dupe_key in dupes:
-                    finding = dupes[dupe_key]
-                    finding.nb_occurences += 1
-                else:
-                    finding = Finding(
-                        title=f"{type}",
-                        test=test,
-                        description=description,
-                        cwe=798,
-                        date=find_date,
-                        severity="High",
-                        verified=is_verified,
-                        active="is_secret" in item
-                        and item["is_secret"] is True
-                        or "is_secret" not in item,
-                        file_path=file,
-                        line=line,
-                        nb_occurences=1,
-                        false_p="is_secret" in item
-                        and item["is_secret"] is False,
-                    )
-                    dupes[dupe_key] = finding
-        return list(dupes.values())
+        for file_path, file_issues in issues.items():
+            for issue in file_issues:
+                line = issue["line"]
+                string = issue["string"]
+                line_number = issue["line_number"]
+                rule = issue["rule"]
+                reason = issue["reason"]
+                fingerprint = issue["fingerprint"]
+
+                title = f"Hard Coded {rule} found in {file_path}"
+                description = f"**Reason:** {reason}\n"
+                description += f"**String Found:**\n\n```\n{line}\n```\n"
+                description += f"**Line:** {line_number}\n"
+                description += f"**Fingerprint:** {fingerprint}"
+
+                severity = "Critical"
+
+                finding = Finding(
+                    title=title,
+                    test=test,
+                    cwe=798,
+                    description=description,
+                    severity=severity,
+                    file_path=file_path,
+                    line=line_number,
+                    dynamic_finding=False,
+                    static_finding=True,
+                )
+                findings.append(finding)
+
+        return findings
